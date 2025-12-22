b4c3bad97a65aeb10a3edab041f1a89ba43b5ef7
OsikanyiTheDev <jillskillion@gmail.com>
2025-12-22 14:20:17 +0000

    created factorial calculator
    created a factorial calculator here

---
+++ Small projects/factorialCalculator.js
@@ -0,0 +1,14 @@
+let num = 5;
+
+function factorialCalculator(num){
+  let result = 1;
+  for (let i = 1; i<=num; num--){
+    result *=num;
+  }
+  return result;
+}
+
+let factorial = factorialCalculator(num);
+let resultMsg = `Factorial of ${num} is ${factorial}`
+
+console.log(resultMsg)
