d2a0302d24e18c369c3ff64b8c41405e91151715
OsikanyiTheDev <jillskillion@gmail.com>
2025-12-22 14:52:44 +0000

    created ChunksOfArray.js

---
+++ Small projects/chunksOfArray.js
@@ -0,0 +1,9 @@
+function chunkArrayInGroups (array, num) {
+  let result = [];
+  
+  for (let i = 0; i < array.length; i += num) {
+    result.push(array.slice(i, i + num));
+  }
+  
+  return result;
+}
