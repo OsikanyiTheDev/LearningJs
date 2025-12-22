e1a0fff90cf54ed7aa763f813defec8a75e0890c
OsikanyiTheDev <jillskillion@gmail.com>
2025-12-22 17:06:44 +0000

    Create retrivingContactInformation.js

---
+++ Small projects/retrivingContactInformation.js
@@ -0,0 +1,41 @@
+let contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  for (let i = 0; i < contacts.length; i++) {
+    if (contacts[i].firstName === name) {
+      if (contacts[i].hasOwnProperty(prop)) {
+        return contacts[i][prop]; // return the property value
+      } else {
+        return "No such property"; // property not found
+      }
+    }
+  }
+  return "No such contact"; // name not found
+}
+
+
